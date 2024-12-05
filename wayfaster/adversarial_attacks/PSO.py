e33d38f78b472b4fbc3ea900c980f3e8d7957942
Richard <rzhao5659@gmail.com>
2024-12-05 03:28:40 -0500

    shadow attack optimized

--- wayfaster/adversarial_attacks/PSO.py
+++ wayfaster/adversarial_attacks/PSO.py
@@ -3,38 +3,46 @@ from typing import Callable
 import torch
 import tqdm
 
+# PSO implementation for batch of inputs of size N.
+
 
 class Swarm:
     """
     Manages the state of particles (positions, velocities, personal bests, global best)
-    A particle is a torch.Tensor(dim).
+    Positions, velocities, personal best are shape (batch_size = N, n_particles, 6)
+    Global best is shape (N, 6)
     """
 
-    def __init__(self, n_particles, dim, c1, c2, w, pos_constraint, vel_constraint):
+    def __init__(self, batch_size, n_particles, dim, c1, c2, w, pos_constraint, vel_constraint):
         assert vel_constraint.shape == (2, dim)
         assert pos_constraint.shape == (2, dim)
-
         # Parameters
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.batch_size = batch_size
         self.n_particles = n_particles
         self.dim = dim
         self.c1 = c1
         self.c2 = c2
         self.w = w
-        self.pos_constraint = pos_constraint
-        self.vel_constraint = vel_constraint
+        self.pos_constraint = pos_constraint.to(self.device)
+        self.vel_constraint = vel_constraint.to(self.device)
 
         # Storage:
         # Positions and velocities are initialized randomly and clipped.
-        pos_lower_bound = pos_constraint[0]
-        pos_upper_bound = pos_constraint[1]
-        vel_lower_bound = vel_constraint[0]
-        vel_upper_bound = vel_constraint[1]
-        self.positions = pos_lower_bound + (pos_upper_bound - pos_lower_bound) * torch.rand((n_particles, dim))
-        self.velocities = vel_lower_bound + (vel_upper_bound - vel_lower_bound) * torch.rand((n_particles, dim))
-        self.pbest = torch.ones((n_particles, dim)) * float("inf")
-        self.gbest = torch.ones((1, dim)) * float("inf")
-        self.pbest_costs = torch.ones(n_particles) * float("inf")
-        self.gbest_cost = torch.ones(1) * float("inf")
+        pos_lower_bound = self.pos_constraint[0]
+        pos_upper_bound = self.pos_constraint[1]
+        vel_lower_bound = self.vel_constraint[0]
+        vel_upper_bound = self.vel_constraint[1]
+        self.positions = pos_lower_bound + (pos_upper_bound - pos_lower_bound) * torch.rand(
+            (self.batch_size, self.n_particles, self.dim), device=self.device
+        )
+        self.velocities = vel_lower_bound + (vel_upper_bound - vel_lower_bound) * torch.rand(
+            (self.batch_size, self.n_particles, self.dim), device=self.device
+        )
+        self.pbest = torch.ones((self.batch_size, self.n_particles, self.dim)).to(self.device) * float("inf")
+        self.gbest = torch.ones((self.batch_size, self.dim)).to(self.device) * float("inf")
+        self.pbest_costs = torch.ones((self.batch_size, self.n_particles)).to(self.device) * float("inf")
+        self.gbest_cost = torch.ones(self.batch_size).to(self.device) * float("inf")
 
     def reset(self):
         """
@@ -44,17 +52,16 @@ class Swarm:
         pos_upper_bound = self.pos_constraint[1]
         vel_lower_bound = self.vel_constraint[0]
         vel_upper_bound = self.vel_constraint[1]
-
         self.positions = pos_lower_bound + (pos_upper_bound - pos_lower_bound) * torch.rand(
-            (self.n_particles, self.dim)
+            (self.batch_size, self.n_particles, self.dim), device=self.device
         )
         self.velocities = vel_lower_bound + (vel_upper_bound - vel_lower_bound) * torch.rand(
-            (self.n_particles, self.dim)
+            (self.batch_size, self.n_particles, self.dim), device=self.device
         )
-        self.pbest = torch.ones((self.n_particles, self.dim)) * float("inf")
-        self.gbest = torch.ones((1, self.dim)) * float("inf")
-        self.pbest_costs = torch.ones(self.n_particles) * float("inf")
-        self.gbest_cost = torch.ones(1) * float("inf")
+        self.pbest = torch.ones((self.batch_size, self.n_particles, self.dim)).to(self.device) * float("inf")
+        self.gbest = torch.ones((self.batch_size, self.dim)).to(self.device) * float("inf")
+        self.pbest_costs = torch.ones((self.batch_size, self.n_particles)).to(self.device) * float("inf")
+        self.gbest_cost = torch.ones(self.batch_size).to(self.device) * float("inf")
 
     def update_positions(self):
         """Update all particles' positions"""
@@ -63,16 +70,15 @@ class Swarm:
 
     def update_velocities(self):
         """Update all particles' velocities"""
-
         # Generate random weights in [0,1]
-        rand_1 = torch.rand((self.n_particles, self.dim))
-        rand_2 = torch.rand((self.n_particles, self.dim))
+        rand_1 = torch.rand((self.batch_size, self.n_particles, self.dim), device=self.device)
+        rand_2 = torch.rand((self.batch_size, self.n_particles, self.dim), device=self.device)
 
         # Update velocities
         self.velocities = (
             self.w * self.velocities
             + self.c1 * rand_1 * (self.pbest - self.positions)
-            + self.c2 * rand_2 * (self.gbest - self.positions)
+            + self.c2 * rand_2 * (self.gbest.unsqueeze(1).expand(-1, self.n_particles, -1) - self.positions)
         )
         self.velocities = self.velocities.clip(self.vel_constraint[0], self.vel_constraint[1])
 
@@ -80,6 +86,7 @@ class Swarm:
 class PSO:
     def __init__(
         self,
+        batch_size,
         num_iters,
         n_particles,
         dim,
@@ -89,32 +96,35 @@ class PSO:
         pos_constraint,
         vel_constraint,
     ):
-        self.swarm = Swarm(n_particles, dim, c1, c2, w, pos_constraint, vel_constraint)
+        self.swarm = Swarm(batch_size, n_particles, dim, c1, c2, w, pos_constraint, vel_constraint)
         self.num_iters = num_iters
 
     def optimize(self, cost_fn: Callable):
         """
-        Find best particle (minimizer) based on the specified cost function that evaluates a particle's cost.
+        Find best particle (minimizer) based on the specified particle cost function that accepts a batch of particles.
         Terminates by number of iterations.
+        Args:
+            cost_fn: Function that returns the cost for a batch of particles with shape (*, dim)
+        Return:
+            (self.swarm.gbest_cost, self.swarm.gbest) where gbest_cost of shape (N, ) and gbest of shape (N, dim)
         """
-
         # Reinitialize swarm
         self.swarm.reset()
 
         for _ in tqdm.tqdm(range(self.num_iters)):
-            # Evaluate all particles's costs at their current positions, and update their personal bests.
-            for j in range(self.swarm.n_particles):
-                # Compute its cost
-                particle_cost = cost_fn(self.swarm.positions[j])
-                # Update personal best
-                if particle_cost < self.swarm.pbest_costs[j]:
-                    self.swarm.pbest_costs[j] = particle_cost
-                    self.swarm.pbest[j] = self.swarm.positions[j]
-                # Update global best
-                if self.swarm.pbest_costs[j] < self.swarm.gbest_cost:
-                    self.swarm.gbest_cost = self.swarm.pbest_costs[j]
-                    self.swarm.gbest = self.swarm.positions[j]
-
+            # Reshape particles positions from (N, n_particles, dim) to (N * n_particles, dim)
+            particles_pos = self.swarm.positions.view(-1, self.swarm.dim)
+            # Evaluate cost at their current positions
+            particles_cost = cost_fn(particles_pos)
+            particles_cost = particles_cost.view(self.swarm.batch_size, self.swarm.n_particles)
+            # Update personal best (N, n_particles)
+            update_mask = particles_cost < self.swarm.pbest_costs
+            self.swarm.pbest_costs = torch.where(update_mask, particles_cost, self.swarm.pbest_costs)
+            self.swarm.pbest = torch.where(update_mask.unsqueeze(-1), self.swarm.positions, self.swarm.pbest)
+            # Update global best (N,)
+            batch_best_cost, batch_best_idx = self.swarm.pbest_costs.min(dim=1)
+            self.swarm.gbest_cost = torch.min(self.swarm.gbest_cost, batch_best_cost)
+            self.swarm.gbest = self.swarm.pbest[torch.arange(self.swarm.batch_size), batch_best_idx]
             # Update all particles's positions and velocities.
             self.swarm.update_velocities()
             self.swarm.update_positions()
---
+++ wayfaster/eval_shadow_attack.py
@@ -0,0 +1,177 @@
+import torch
+import pytorch_lightning as pl
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+from torch.utils.data import DataLoader
+from pytorch_lightning.loggers import WandbLogger
+
+# Custom packages
+from train.dataloader import Dataset
+from train.train_configs import get_cfg
+from train.trainer import TrainingModule
+from models.traversability_net import TravNet
+from adversarial_attacks.shadow_attacker_optimized import ShadowAttack
+import tqdm
+import wandb
+
+
+# RUN COMMAND:  python3 wayfaster/eval_shadow_attack.py
+
+# Parameters. For quick testing, just evaluate on val
+BATCH_SIZE = 5
+TRAIN_BATCH_END_IDX = 1
+LOGGER_RUN_NAME = "shadow_attack_eval"
+EVALUATE_ON_TRAIN = False
+EVALUATE_ON_VAL = True
+
+# Note: I wanted to log images at step = data index but its impossible with wandb nor WandbLogger. I give up (I tried, but its their internal step tracker that fucks it up)
+# Therefore this evaluation script contains no index information, although you can still see results on wandb.ai, just step not related to index,
+
+
+def visualize_input_output(
+    model,
+    image,
+    trav_map,
+    pred_depth,
+    prefix,
+):
+    """It logs a batch of images, with first datapoint logged on step = `start_data_idx`"""
+
+    # Prepare depth_prediction
+    n_d = (model.grid_bounds["dbound"][1] - model.grid_bounds["dbound"][0]) / model.grid_bounds["dbound"][2]
+    depth_pred = torch.argmax(pred_depth, dim=1, keepdim=True) / (n_d - 1)
+    shape = depth_pred.shape
+    depth_pred = depth_pred.unsqueeze(0).view(BATCH_SIZE, 6, *shape[1:])
+
+    for i in range(BATCH_SIZE):
+        # Visualize the camera inputs
+        wandb.log({prefix + "_images": wandb.Image(image[i])})
+        # Visualize the traversability map
+        wandb.log({prefix + "_mu": wandb.Image(trav_map[i, 0])})
+        wandb.log({prefix + "_nu": wandb.Image(trav_map[i, 1])})
+        # Visualize the depth prediction
+        wandb.log({prefix + "_depth_pred": wandb.Image(depth_pred[i])})
+
+
+def load_data(configs):
+    train_dataset, train_loader, valid_dataset, valid_loader = None, None, None, None
+    if EVALUATE_ON_TRAIN:
+        train_dataset = Dataset(configs, configs.DATASET.TRAIN_DATA)
+        train_loader = DataLoader(
+            train_dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+            drop_last=True,
+            pin_memory=True,
+        )
+    if EVALUATE_ON_VAL:
+        valid_dataset = Dataset(configs, configs.DATASET.VALID_DATA)
+        valid_loader = DataLoader(
+            valid_dataset,
+            batch_size=BATCH_SIZE,
+            shuffle=False,
+            drop_last=True,
+            pin_memory=True,
+        )
+    return train_dataset, train_loader, valid_dataset, valid_loader
+
+
+def evaluate_attacker(trav_model, dataloader, attacker, dataloader_prefix: str):
+    """
+    Evaluate adversarial attacks on the pretrained traversability network model for some dataloader.
+    It will log results on WANDB with step (x-axis) being the datapoint index (not epoch).
+    """
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    with torch.no_grad():
+        trav_model.eval()
+
+        for batch_idx, batch in tqdm.tqdm(enumerate(dataloader)):
+            if batch_idx == TRAIN_BATCH_END_IDX:
+                break
+            # Unperturbed data:
+            color_img, pcloud, inv_intrinsics, extrinsics, path, target_trav, trav_weights, depth_target, depth_mask = batch  # fmt: skip
+
+            # Load them to the right device
+            color_img = color_img.to(device)
+            pcloud = pcloud.to(device)
+            inv_intrinsics = inv_intrinsics.to(device)
+            extrinsics = extrinsics.to(device)
+
+            trav_map, pred_depth, _ = trav_model(color_img, pcloud, inv_intrinsics, extrinsics)
+            visualize_input_output(
+                trav_model,
+                color_img,
+                trav_map,
+                pred_depth,
+                prefix=dataloader_prefix + "_unperturbed",
+            )
+
+            # Perturb data by shadow attack
+            # mean_abs_error is of shape (batch_size, ) as it simply compares unperturbed output and perturbed output.
+            mean_abs_error, perturbed_color_img = attacker.generate_attack(
+                model_inputs=[color_img, pcloud, inv_intrinsics, extrinsics]
+            )
+            print("length of mean abs error", len(mean_abs_error))
+            trav_map, pred_depth, _ = trav_model(perturbed_color_img, pcloud, inv_intrinsics, extrinsics)
+            visualize_input_output(
+                trav_model,
+                perturbed_color_img,
+                trav_map,
+                pred_depth,
+                prefix=dataloader_prefix + "_perturbed",
+            )
+
+            # Log mean_abs_error.
+            for i in range(BATCH_SIZE):
+                wandb.log({dataloader_prefix + "_mean_abs_error": mean_abs_error[i].item()})
+
+
+def main():
+    # Parse config file
+    CONFIG_FILE_PATH = "configs/temporal_model.yaml"
+    configs = get_cfg(CONFIG_FILE_PATH)
+
+    # Set seed and device
+    pl.seed_everything(configs.SEED, workers=True)
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    # Load data
+    train_dataset, train_loader, valid_dataset, valid_loader = load_data(configs)
+
+    # Load pretrained model and logger (it's pl.lightning module, model.model is the actual traversability network)
+    model = TrainingModule(configs)
+    # wandb_logger = WandbLogger(project=LOGGER_RUN_NAME)
+    wandb.init(project=LOGGER_RUN_NAME)
+
+    if configs.MODEL.LOAD_NETWORK is not None:
+        print("Loading saved network from {}".format(configs.MODEL.LOAD_NETWORK))
+        pretrained_dict = torch.load(configs.MODEL.LOAD_NETWORK, map_location="cpu")["state_dict"]
+        model.load_state_dict(pretrained_dict)
+        model = model.to(device)  # Load model to cuda
+
+    # Extract actual traversability model
+    trav_model: TravNet = model.model
+    trav_model = trav_model.to(device)
+
+    # Set up adversarial attacker (Don't change parameters, its fast but at the expense of a lot of memory... will crash your PC, I will run this on cluster.)
+    attacker = ShadowAttack(trav_model)
+    attacker.PSO_params["num_iters"] = 2
+    attacker.PSO_params["n_particles"] = 2
+    print("Shadow Attack PSO Parameters: ")
+    print(attacker.PSO_params)
+
+    # Evaluate adversarial attacks on train set (results are logged)
+    if EVALUATE_ON_TRAIN:
+        evaluate_attacker(trav_model, train_loader, attacker, dataloader_prefix="train")
+
+    # Evaluate adversarial attacks on validation set (results are logged)
+    if EVALUATE_ON_VAL:
+        evaluate_attacker(trav_model, valid_loader, attacker, dataloader_prefix="val")
+
+
+if __name__ == "__main__":
+    main()
